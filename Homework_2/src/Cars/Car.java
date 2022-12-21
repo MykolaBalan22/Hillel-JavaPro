811c3ba909bf2402101187a48f82cc4dcd3100bb
Mykola Balan <balann705@gmail.com>
2022-12-21 14:49:16 +0200

    Created Car class . Added start method and some private methods

---
+++ Homework_2/src/Cars/Car.java
@@ -0,0 +1,18 @@
+package Cars;
+
+public class Car {
+    public void start (){
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+    }
+
+    private void startFuelSystem() {
+    }
+
+    private void startCommand() {
+    }
+
+    private void startElectricity() {
+    }
+}
