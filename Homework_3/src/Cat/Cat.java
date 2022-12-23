30e8d8285700f99c9c259773601da2ec4eaa816f
Mykola Balan <balann705@gmail.com>
2022-12-23 17:36:22 +0200

    Change Cat class . Added constructor and override methods

--- Homework_3/src/Cat/Cat.java
+++ Homework_3/src/Cat/Cat.java
@@ -2,6 +2,30 @@ package Cat;
 
 import Animal.Animal;
 
-public class Cat  {
+public class Cat extends  Animal{
+    public Cat(String catName) {
+        Animal.addNewAnimal();
+        if (catName!=null) {
+            this.nameAnimal=catName;
+        }else {
+            this.nameAnimal="Безіменний";
+        }
+    }
 
+
+    @Override
+    public  void run(double length){
+        if (length<=200 && length>=0) {
+            System.out.printf("%s пробігла %.2f м.\n",nameAnimal ,length);
+        }else if(length<0) {
+            System.out.printf("Некоректа відсань %.2f м.(коти в мінус не бігають)\n",length);
+        }else if(length>200 ){
+            System.out.printf("Некоректа відсань %.2f м.(коти стільки  не бігають , забагато)\n", length);
+        }
+
+    }
+    @Override
+    public  void swim(double length){
+        System.out.println("Коти не вміють плавати !");
+    }
 }
