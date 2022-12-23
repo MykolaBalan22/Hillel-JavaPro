dab7a03e946739cff2f3e59e556c87bc97225893
Mykola Balan <balann705@gmail.com>
2022-12-23 17:34:40 +0200

    Change Animal class . Added counter and some methods

--- Homework_3/src/Animal/Animal.java
+++ Homework_3/src/Animal/Animal.java
@@ -1,5 +1,14 @@
 package Animal;
 
 public abstract class Animal {
-
+    private    static  int  counterAnimals=0 ;
+    protected   String nameAnimal ;
+    public  abstract void run(double length);
+    public  abstract void swim(double length);
+    public static void showAmountAnimals(){
+        System.out.printf("Created : %d animals \n", counterAnimals);
+    }
+    protected static void addNewAnimal(){
+        Animal.counterAnimals++;
+    }
 }
