827ca1fbb4b5d3e0eb35f10c6f9bb412b380f07e
Mykola Balan <balann705@gmail.com>
2023-01-04 18:38:45 +0200

    Added toList method .

--- Homework_4/src/ListTester/ListTester.java
+++ Homework_4/src/ListTester/ListTester.java
@@ -16,4 +16,15 @@ public class ListTester {
         }
         return counter;
     }
+    /**
+     * Метод toList, що приймає на вхід цілісний масив довільної довжини.
+     * Конвертувати масив у список відповідного типу даних та повернути з методу
+     * **/
+    public static List<?> toList (Object [] inputSequense){
+        List<Object> outputArray = new ArrayList<>();
+        for (Object o : inputSequense) {
+            outputArray.add(o);
+        }
+        return outputArray ;
+    }
 }
