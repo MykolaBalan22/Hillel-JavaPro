0cd41c43178e591bb9928389b9938d09f566cedd
Mykola Balan <balann705@gmail.com>
2023-01-16 19:54:17 +0200

    Create method add in FileNavigator class . Add some overrides methods in Filedata class.

--- Homework_5/src/FilePackage/FileData.java
+++ Homework_5/src/FilePackage/FileData.java
@@ -1,9 +1,64 @@
 package FilePackage;
 
+import java.util.Objects;
+
 public class FileData {
     private String fileName ;
     private long sizeOfFile;
     private String path;
 
+    public FileData(String fileName, long sizeOfFile, String path) {
+        this.fileName = fileName;
+        this.sizeOfFile = sizeOfFile;
+        this.path = path;
+    }
 
+    public FileData() {
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public long getSizeOfFile() {
+        return sizeOfFile;
+    }
+
+    public void setSizeOfFile(long sizeOfFile) {
+        this.sizeOfFile = sizeOfFile;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FileData fileData = (FileData) o;
+        return sizeOfFile == fileData.sizeOfFile && Objects.equals(fileName, fileData.fileName) && Objects.equals(path, fileData.path);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fileName, sizeOfFile, path);
+    }
+
+    @Override
+    public String toString() {
+        return "FileData{" +
+                "fileName='" + fileName + '\'' +
+                ", sizeOfFile=" + sizeOfFile +
+                ", path='" + path + '\'' +
+                '}';
+    }
 }
--- Homework_5/src/FilePackage/FileNavigator.java
+++ Homework_5/src/FilePackage/FileNavigator.java
@@ -1,9 +1,25 @@
 package FilePackage;
 
 import java.util.HashMap;
+import java.util.HashSet;
+
 
 public class FileNavigator {
-    private String path ;
-    private HashMap<String ,FileData>  filesInDirectory ;
+    private HashMap<String,HashSet<FileData>> filesInDirectory ;
 
+    public FileNavigator() {
+        this.filesInDirectory = new HashMap<>();
+    }
+    public void add (FileData file){
+        if (filesInDirectory.containsKey(file.getPath())==false) {
+            filesInDirectory.put(file.getPath(),new HashSet<>());
+        }
+        filesInDirectory.get(file.getPath()).add(file);
+    }
+    @Override
+    public String toString() {
+        return "FileNavigator{" +
+                "filesInDirectory=" + filesInDirectory +
+                '}';
+    }
 }
