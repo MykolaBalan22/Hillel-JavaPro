17b22333ba2a2290e68c29072d100042547fbd9f
Mykola Balan <balann705@gmail.com>
2023-03-05 19:47:30 +0200

    Created ListNode ,Reverser classes and static reverseList method .Also wrote test for reverseList method .

---
+++ Homework_10/src/main/java/task2/Reverser.java
@@ -0,0 +1,13 @@
+package task2;
+
+public class Reverser {
+    public static ListNode reverseList(ListNode head) {
+        ListNode nextNode = head;
+        ListNode newNode = null;
+        while (nextNode != null) {
+            newNode = new ListNode(nextNode.val, newNode);
+            nextNode = nextNode.next;
+        }
+        return newNode;
+    }
+}
