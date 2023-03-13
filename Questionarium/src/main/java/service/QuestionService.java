d60b20e0843580c42a5d14092af62a529d5b5df4
Mykola Balan <balann705@gmail.com>
2023-03-13 17:47:30 +0200

    Created QuestionService class.

---
+++ Questionarium/src/main/java/service/QuestionService.java
@@ -0,0 +1,21 @@
+package service;
+
+import model.Question;
+import repository.dao.QuestionRepository;
+
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class QuestionService {
+    private QuestionRepository repository;
+
+    public QuestionService(QuestionRepository repository) {
+        this.repository = repository;
+    }
+
+    public Question getRndQuestoinByTopic(String topic) {
+        List<Question> topics = repository.getByTopic(topic);
+        int randomNum = ThreadLocalRandom.current().nextInt(0, topics.size());
+        return topics.get(randomNum);
+    }
+}
